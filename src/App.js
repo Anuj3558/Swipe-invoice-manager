9037cdb4cee1aa6919da29c267056a182867a7d5
Anuj3558 <anujloharkar3557@gmail.com>
2024-11-20 00:06:10 +0530

    Forward

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+
+import UploadPage from './components/dashboard/dashboard';
 
 function App() {
   return (
-    <LandingPage />
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/upload" element={<UploadPage />} />
+        
+      </Routes>
+    </Router>
   );
 }
 
